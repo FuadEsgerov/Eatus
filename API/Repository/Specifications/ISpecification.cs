08b599d544ae2cb0a37e8166937d2382188b365f
FuadEsgerov <fuadra@code.edu.az>
2020-07-13 23:08:14 +0400

    Creating Specifications

---
+++ API/Repository/Specifications/ISpecification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Specifications
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+        int Take { get; }
+        int Skip { get; }
+        bool IsPagingEnabled { get; }
+    }
+}
---
+++ API/Repository/Specifications/ProductWithFiltersForCountSpecificication.cs
@@ -0,0 +1,17 @@
+
+using Repository.Entities;
+
+namespace Repository.Specifications
+{
+    public class ProductWithFiltersForCountSpecificication : BaseSpecification<Product>
+    {
+        public ProductWithFiltersForCountSpecificication(ProductSpecParams productParams) 
+            : base(x => 
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
+        {
+        }
+    }
+}
